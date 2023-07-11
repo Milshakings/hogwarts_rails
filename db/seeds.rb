410a6662bc6d70afbb02c42b4600ddf5637d3c7f
Milshakings <galleguillosf.raul@gmail.com>
2023-07-11 12:02:30 -0400

    Nueva actualizacion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do |i|
+    Character.create(
+      name: Faker::Name.name,
+      location: Faker::Address.location,
+      house: Faker::House.house)
+    
+  end
